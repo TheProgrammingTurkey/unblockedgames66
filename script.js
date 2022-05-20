aac8cc351d78bed885700687cce3a4c985231202
Glitch (reminiscent-remarkable-mousepad) <none>
2022-05-20 15:59:22 +0000

    Import of turkey2098/unblockedgames66

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+if (!location.protocol[5]) {
+  location.protocol = "https:";
+}
+document.querySelector("img").onclick = function () {
+  window.location.href = "/index.html";
+};
+
+var game = document.getElementById("iframe");
+function openFullscreen() {
+  if (game.requestFullscreen) {
+    game.requestFullscreen();
+  } else if (game.webkitRequestFullscreen) {
+    /* Safari */
+    game.webkitRequestFullscreen();
+  } else if (game.msRequestFullscreen) {
+    /* IE11 */
+    game.msRequestFullscreen();
+  }
+}
+
+function search_games() {
+  let input = document.getElementById("searchbar").value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("games");
+
+  for (var i = 0; i < x.length; i++) {
+    if (!x[i].textContent.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "list-item";
+    }
+  }
+}
